47feb655821a887a0785faa153a95e9d3bab1363
棠筱柒 <1726567547@qq.com>
2024-07-03 15:03:12 +0800

    更新

--- src/renderer/src/views/content/index.vue
+++ src/renderer/src/views/content/index.vue
@@ -3,7 +3,8 @@
         <template v-for="item in dataArr">
             <el-card style="max-width: 280px">
                 <el-text line-clamp="2" size="large" style="font-weight: bold;">{{ item.title }}</el-text><br>
-                <el-text line-clamp="3" size="Small" style="color:rgb(130, 130, 130);width: 100%;">{{ item.Content }}</el-text>
+                <el-text line-clamp="3" size="Small" style="color:rgb(130, 130, 130);width: 100%;">{{ item.Content
+                    }}</el-text>
                 <img :src="item.cover" style="width: 70%;height: 70%;" />
                 <!-- <div class="image-container" style="width: 300px; height: 70%; overflow: hidden;">
                     <div class="image"
@@ -21,7 +22,8 @@ import { ref, onMounted } from 'vue'
 const dataArr = ref([]);
 
 onMounted(() => {
-    Api.DB.GetContent(1).then(datas => {
+    let index = 1;
+    Api.DB.GetContent(index).then(datas => {
         datas.forEach(data => {
             dataArr.value.push({
                 title: data.Title,
@@ -30,6 +32,18 @@ onMounted(() => {
             });
         });
     });
+    setInterval(() => {
+        index += 1;
+        Api.DB.GetContent(index).then(datas => {
+            datas.forEach(data => {
+                dataArr.value.push({
+                    title: data.Title,
+                    cover: "data:image/png;base64," + data.Cover,
+                    Content: data.ContentText
+                });
+            });
+        });
+    }, 5000);
 })
 </script>
 
